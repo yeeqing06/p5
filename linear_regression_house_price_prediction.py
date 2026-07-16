7aeff17f557f1ef04df16ce1134c0ae18d317c93
yeeqing06 <ngyq-wp24@student.tarc.edu.my>
2026-07-16 13:15:10 +0800

    Update linear_regression_house_price_prediction.py

--- linear_regression_house_price_prediction.py
+++ linear_regression_house_price_prediction.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1sAv6W8GI0JB9b54EN3y8rdbIpL4hbrXC
 """
 
-import streamlit as st
+import streamlit as st 
 from joblib import load
 import numpy as np
 
@@ -15,12 +15,12 @@ import numpy as np
 model = load('linear_regression_model.joblib')
 
 # Create a simple user input
-user_input = st.number_input('Enter house size:', min_value = 100, max_value = 10000, step = 50)
+user_input = st.number_input('Enter house size:', min_value = 100, max_value = 10000, step =50)
 
 # Reshape the input for the model
 input_array = np.array([user_input]).reshape(-1,1)
 
 # Predict the house price
-if(st.button('Predict Price'):
-    predicted_price = model.predict(input_array)
-    st.write(f"Predicted Price: ${predicted_price[0]:.2f}")
+if st.button('Predict Price'):
+  predicted_price = model.predict(input_array)
+  st.write(f"The predicted house price is:${predicted_price[0]:.2f}")
